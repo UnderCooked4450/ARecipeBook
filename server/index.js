40d594865b10d553a219ac160e1dc16be9338ced
SplendidTea <sharl@DESKTOP-5OVL70D>
2023-11-24 17:43:05 -0500

    Server to connect to Mongo inital version

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+require('dotenv').config();
+
+
+const app = express();
+
+
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString,{dbName: 'ArCooked'});
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.use('/api/', routes)
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
---
+++ server/routes/routes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const Model = require('../model/model');
+
+const router = express.Router()
+
+//Post Method
+router.post('/post', async (req, res) => {
+    const data= new Model({
+        name: req.query.name,
+        ingredients: req.query.ingredients,
+        vegetarian: req.query.vegetarian,
+        filters: req.query.filters
+    })
+
+    try{
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }catch(e)
+    {
+        res.status(400).json({message: error.message})
+    }   
+})
+
+//Get all Method
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+module.exports = router;
