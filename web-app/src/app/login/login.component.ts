e30808a8ac4f85c8bdcb59b1db4928ced95815d4
ppate343 <ppate343@uwo.ca>
2023-12-30 21:24:38 -0500

    register toggle logic

--- web-app/src/app/login/login.component.ts
+++ web-app/src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RegisterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isShowLogin = false; 
+  isShowRegister = true; 
+  toggleViewLogin() {  
+    this.isShowLogin = false; 
+    this.isShowRegister = true; 
+  }  
+  toggleViewRegister() {  
+    this.isShowLogin = true; 
+    this.isShowRegister = false;  
+  }  
 
 }
