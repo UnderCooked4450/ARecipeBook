7977d7da8ca25d7fa5f7e0840ba60b271fb66b38
Brianna0510 <65310388+Brianna0510@users.noreply.github.com>
2024-01-25 18:10:29 -0500

    homepage layout

---
+++ web-app/src/environments/environment_product.ts
@@ -0,0 +1,10 @@
+
+// this is from devlopement. Need to setup a new environment for product
+export const firebaseConfig = {
+    apiKey: "AIzaSyCocIDIJThS9sKKMLGdEx-kOKnAgStJqV4",
+    authDomain: "arecipe-development.firebaseapp.com",
+    projectId: "arecipe-development",
+    storageBucket: "arecipe-development.appspot.com",
+    messagingSenderId: "622388583287",
+    appId: "1:622388583287:web:77f3910c35768a7114df72"
+  };
