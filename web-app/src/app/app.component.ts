bca71973151f6c52164d8e53f5ef55eef2090400
ppate343 <ppate343@uwo.ca>
2024-01-29 10:36:09 -0500

    removed styles ref

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: [],
 })
 export class AppComponent {
   title = 'web-app';
