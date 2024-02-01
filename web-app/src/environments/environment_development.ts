d2fd267b614d836ca38cd6f79a777f5e6ed2991f
Brianna0510 <65310388+Brianna0510@users.noreply.github.com>
2024-02-01 15:30:49 -0500

    removed environment folder from tracking

--- web-app/src/environments/environment_development.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCocIDIJThS9sKKMLGdEx-kOKnAgStJqV4",
-    authDomain: "arecipe-development.firebaseapp.com",
-    projectId: "arecipe-development",
-    storageBucket: "arecipe-development.appspot.com",
-    messagingSenderId: "622388583287",
-    appId: "1:622388583287:web:77f3910c35768a7114df72"
-  };
