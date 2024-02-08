cf4aa1d02851c05a94b50d9bcb398efdf1714318
Nick <nicholasgin@gmail.com>
2024-02-08 16:33:42 -0500

    added routes for camera page and some camera functionality

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -7,15 +7,11 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './app.component.html'
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   // ,styleUrls: ['./app.component.css'],
-=======
   // styleUrls: ['./app.component.css'],
->>>>>>> 9b69d0a (Added camera page)
-=======
+
   // styleUrls: ['./app.component.css'],
->>>>>>> 9b69d0a58f8827e3ae77b1c3732e00511fc84330
 })
 export class AppComponent {
   title = 'web-app';
---
+++ web-app/src/app/camera/camera.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'camera',
+  standalone: true,
+  imports: [FormsModule], //TODO camera library
+  templateUrl: './camera.component.html',
+  styleUrl: './camera.component.css',
+})
+export class CameraComponent {
+  constructor(private router: Router) {}
+
+  video = document.getElementById('video') as HTMLVideoElement;
+
+  openCamera() {
+    try {
+        //check  if the browser supports getUserMedia api
+        if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
+            console.log("camera time")
+            navigator.mediaDevices.getUserMedia({video: true})
+            .then((stream) => {
+                this.video.srcObject = stream;
+            })
+            .catch((error) => {
+                console.log("Error: " + error)
+            })
+
+            navigator.mediaDevices.getUserMedia({
+                video: {
+                    width: {ideal: 1920},
+                    height: {ideal: 1080},
+                    facingMode: 'user'
+                }
+            })
+        }
+    } catch (error) {
+      alert('Error accessing the camera and microphone');
+      console.error(error);
+    }
+  }
+}
