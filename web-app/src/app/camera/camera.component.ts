75e167d2dea6aab1676695e0cf31e9a9a3ff319f
Nick <nicholasgin@gmail.com>
2024-02-08 17:15:33 -0500

    added ngx webcam api, added request camera access, and open camera

--- web-app/src/app/camera/camera.component.ts
+++ web-app/src/app/camera/camera.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { WebcamModule } from 'ngx-webcam';
 
 @Component({
   selector: 'camera',
   standalone: true,
-  imports: [FormsModule], //TODO camera library
+  imports: [WebcamModule],
   templateUrl: './camera.component.html',
   styleUrl: './camera.component.css',
 })
@@ -16,25 +16,29 @@ export class CameraComponent {
 
   openCamera() {
     try {
-        //check  if the browser supports getUserMedia api
-        if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-            console.log("camera time")
-            navigator.mediaDevices.getUserMedia({video: true})
-            .then((stream) => {
-                this.video.srcObject = stream;
-            })
-            .catch((error) => {
-                console.log("Error: " + error)
-            })
+      //check  if the browser supports getUserMedia api
+      if (
+        'mediaDevices' in navigator &&
+        'getUserMedia' in navigator.mediaDevices
+      ) {
+        console.log('camera time');
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then((stream) => {
+            this.video.srcObject = stream;
+          })
+          .catch((error) => {
+            console.log('Error: ' + error);
+          });
 
-            navigator.mediaDevices.getUserMedia({
-                video: {
-                    width: {ideal: 1920},
-                    height: {ideal: 1080},
-                    facingMode: 'user'
-                }
-            })
-        }
+        navigator.mediaDevices.getUserMedia({
+          video: {
+            width: { ideal: 1920 },
+            height: { ideal: 1080 },
+            facingMode: 'user',
+          },
+        });
+      }
     } catch (error) {
       alert('Error accessing the camera and microphone');
       console.error(error);
