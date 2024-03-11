9e354fa82e0195d17fc9cd78b2ea96410e6c85e6
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-03-11 02:47:26 -0400

    Table displays added ingredients
    Adding Ingredients functionality works and ingredients are displayed in a table on the same page. Still needs styling but I'm gonna ask for artistic help on that one

--- web-app/src/app/ingredients-page/ingredients-page.component.ts
+++ web-app/src/app/ingredients-page/ingredients-page.component.ts
@@ -1,47 +1,51 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ingredients-page',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './ingredients-page.component.html',
   styleUrl: './ingredients-page.component.css'
 })
 export class IngredientsPageComponent {
+  //quantityType: string | null = null;
+  ingredientList: {ingredientName: string, quantity: number, quantityType: string}[];
+  //listContainer: HTMLElement | null;
 
   constructor(private router: Router) {
+    this.ingredientList = [];
+    console.log(document);
   }
   
   ingredientInput = ''; 
   quantityInput = 0;//default vars for input
   quantityType = '';
-
   ingredientValid = true;
 
   addIngredient() {
       //access name field, quantity field, drop down value
       const ingredient = {
-        name: this.ingredientInput,
+        ingredientName: this.ingredientInput,
         quantity: this.quantityInput, // Get from the form input
         quantityType: this.quantityType
       };
       //validate input
-      this.ingredientValid = this.validateInput(ingredient.name, ingredient.quantity, ingredient.quantityType)
+      this.ingredientValid = this.validateInput(ingredient.ingredientName, ingredient.quantity, ingredient.quantityType)
 
-//should we add more quantity types (ml, g, etc.) and automatic type conversion?
-
+//??? should we add more quantity types (ml, g, etc.) and automatic type conversion ???
 
       if (this.ingredientValid) {
-        console.log(
-          "ingredient name: " + ingredient.name + 
-          "\nquantity: " + ingredient.quantity + 
-          "\nquantity type: " + ingredient.quantityType
-        )
+        //if valid, add ingredient to the list
+        this.ingredientList.push(ingredient)
+        //redraw the table of ingredients
+        //happens automatically thanks to 2 way data binding in angular
       }
   }
 
+  //pretty self explanantory
   validateInput(ingredientName: string, quantity: number, quantityType: string) {
     if (ingredientName.length == 0) {
       alert("Ingredient Name cannot be empty")
@@ -68,7 +72,9 @@ export class IngredientsPageComponent {
   }
 
   generateRecipes() {
-    this.router.navigate(['/ingredientsPage']);
-    console.log("ingredient button pushed");
+    //ingredientList.write to file
+    //or send it to the new page or something, idk, ingredientList has all the ingredient objects
+    this.router.navigate(['/ingredientsPage'/*replace me Puja*/]);
+    console.log("generate receipes button pushed");
   }
 }
