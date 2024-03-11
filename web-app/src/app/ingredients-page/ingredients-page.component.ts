c6dd299c79973e4bae3fc1bec230eab93db6cda0
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-03-11 00:19:25 -0400

    Basic Input Functionality
    Basic input function works and can be verified in browser inspector via console.log. Next is to validate input before logging, then save it to temporary json file or object, then style the page!

--- web-app/src/app/ingredients-page/ingredients-page.component.ts
+++ web-app/src/app/ingredients-page/ingredients-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ingredients-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ingredients-page.component.html',
   styleUrl: './ingredients-page.component.css'
 })
@@ -13,6 +14,29 @@ export class IngredientsPageComponent {
   constructor(private router: Router) {
   }
   
+  ingredientInput = ''; 
+  quantityInput = '';//default vars for input
+  quantityType = '';
+
+  addIngredient() {
+    //display input fields
+      //name, quantity, drop down quantity-type
+      //access name field, quantity field, drop down value
+      //ensure non-null and valid input
+      const ingredient = {
+        name: this.ingredientInput, // Get from the form input
+        quantity: this.quantityInput, // Get from the form input
+        quantityType: this.quantityType
+      };
+     /* if (ingredient.name == null) {
+
+      }*/
+      console.log(
+        "ingredient name: " + ingredient.name + 
+        "\nquantity: " + ingredient.quantity + 
+        "\nquantity type: " + ingredient.quantityType
+      )
+  }
 
   generateRecipes() {
     this.router.navigate(['/ingredientsPage']);
