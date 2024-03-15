56744f1b8decf58d165e18d44a9f118b9ca2f196
Brianna0510 <65310388+Brianna0510@users.noreply.github.com>
2024-03-14 22:22:14 -0400

    remove node_modules

--- web-app/src/app/auth.service.ts
+++ web-app/src/app/auth.service.ts
@@ -33,9 +33,7 @@ export class AuthService {
       })
     );
   }
-  //for recipe searchw
 
-// Inside your recipe service or wherever you make the HTTP call
 searchRecipes(ingredients: string[]): Observable<Array<{ title: string, url: string }>> {
   const backendUrl = 'http://localhost:3000/search'; // Backend URL
   return this.http.post<Array<{ title: string, url: string }>>(backendUrl, { ingredients });
